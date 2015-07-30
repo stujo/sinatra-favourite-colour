231190ee97288cd14e950b099c275cfe894c6335
Hunter Chapman & Stujo <pair+bootcoder.stujo@devbootcamp.com>
2015-07-30 16:12:41 -0700

    Vote Add / Remove

--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@ require 'sinatra/activerecord'
 require 'sinatra'
 require_relative 'environment'
 
+helpers do
+  def hello
+    puts "Hello"
+  end
+end
+
 get '/' do
   redirect '/colors'
 end
 
 get '/colors' do
+  hello
   "hello colors"
 end
 
--- models/color.rb
+++ models/color.rb
@@ -1,4 +1,14 @@
 class Color < ActiveRecord::Base
 
+  def add_vote
+    Color.increment_counter(:vote_count, self.id)
+    reload
+  end
+
+  def remove_vote
+    Color.decrement_counter(:vote_count, self.id)
+    reload
+  end
+
 end
 
